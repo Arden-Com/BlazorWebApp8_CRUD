55cceef20aae200bfc6e7eaafa75c22006926b71
Derek Peacock <derek.peacock@yahoo.com>
2024-04-19 19:14:15 +0100

    Add fields to ApplicationUser

--- BlazorWebApp8_CRUD/BlazorWebApp8_CRUD/Data/ApplicationUser.cs
+++ BlazorWebApp8_CRUD/BlazorWebApp8_CRUD/Data/ApplicationUser.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorWebApp8_CRUD.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "Full Name"), StringLength(30)]
+        public string? FullName { get; set; }
+        [Display(Name = "Birth Date")]
+        public DateOnly? BirthDate { get; set; }
     }
 
 }
---
+++ BlazorWebApp8_CRUD/BlazorWebApp8_CRUD/Data/Migrations/20240419180806_UserFields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWebApp8_CRUD.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+        }
+    }
+}
